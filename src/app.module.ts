a51384c6d07dfc778689b661dbc55312832f003c
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-07-14 13:15:06 +0300

    Feat: Add core modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { VoucherModule } from './voucher/voucher.module';
+import { PaymentModule } from './payment/payment.module';
+import { SmsModule } from './sms/sms.module';
+import { UsageModule } from './usage/usage.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule, VoucherModule, PaymentModule, SmsModule, UsageModule],
   controllers: [AppController],
   providers: [AppService],
 })
